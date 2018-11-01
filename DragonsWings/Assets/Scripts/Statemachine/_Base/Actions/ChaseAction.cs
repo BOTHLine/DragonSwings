fd81a348905467c8b14e9bfc75c42aa8ebfc7b2b
BOTHLine <42834479+BOTHLine@users.noreply.github.com>
2018-11-01 15:28:54 +0100

    First States Setup for enemy

---
+++ DragonsWings/Assets/Scripts/Statemachine/_Base/Actions/ChaseAction.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class ChaseAction : Action
+{
+    public Vector2Reference targetPosition;
+    public Vector2Reference moveDirection;
+
+    public override void Act(StateController controller)
+    {
+        moveDirection.Variable.Value = (targetPosition - (Vector2)controller.transform.position).normalized;
+    }
+
+    public override void EnterState(StateController controller) { }
+    public override void ExitState(StateController controller) { }
+}
---
+++ DragonsWings/Assets/Scripts/Statemachine/_Base/Actions/RepositionAction.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class RepositionAction : Action
+{
+    public Vector2Reference targetPosition;
+    private Vector2 Startposition;
+
+    public override void Act(StateController controller)
+    {
+
+    }
+
+    public override void EnterState(StateController controller) { }
+    public override void ExitState(StateController controller) { }
+}
