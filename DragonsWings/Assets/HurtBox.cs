3ec75262619cc8ad553d2a56e3aa698bc402bb3b
BOTHLine <42834479+BOTHLine@users.noreply.github.com>
2018-11-05 11:21:00 +0100

    Switched Health from Enemy to HurtBox-System

--- DragonsWings/Assets/HurtBox.cs
+++ DragonsWings/Assets/HurtBox.cs
@@ -4,16 +4,22 @@
 [RequireComponent(typeof(Rigidbody2D))]
 public class HurtBox : MonoBehaviour
 {
-    public FloatReference health;
+    public FloatReference healthMax;
+    public FloatReference healthActual;
 
     public GameEvent OnHurt;
     public GameEvent OnDie;
 
+    private void Awake()
+    {
+        healthActual.Variable.Value = healthMax;
+    }
+
     public void Hurt(float damage)
     {
         OnHurt.Raise();
-        health.Variable.Value -= damage;
-        if (health <= 0.0f)
+        healthActual.Variable.Value -= damage;
+        if (healthActual <= 0.0f)
             Die();
     }
 
