8de4feb5589b19866e0f0ee4f4bcdae17b59a6e7
Bürgermeister <freak03@gmx.net>
2018-12-05 20:39:12 +0100

    Demo Für itch überarbeitet
    Tutorialräume hinzugefügt
    Tutorial Texte Hinzugefügt
    Skiptool hinzugefügt
    Hierarchie etwas aufgeräumt
    Bosskampf leichter gemacht ( - 6 Gegner )
    Hookarea härter gemacht ( inkl + 1 Fernkämpfer )
    Kampfraum 4 um Pits ergänzt

---
+++ DragonsWings/Assets/Scripts/Experimental/SkipTheFun.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkipTheFun : MonoBehaviour
+{
+
+    private GameObject player;
+
+
+    public List<GameObject> PlayerPositionList = new List<GameObject>();
+
+    private List<Vector3> positionsList = new List<Vector3>();
+
+
+    public void Start()
+    {
+       player = GameObject.Find("Player");
+
+       positionsList.Add(player.transform.position);
+       positionsList.Add(player.transform.position);
+
+        fillTheList();
+
+    }
+
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            SetSceneKeyPoint(0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SetSceneKeyPoint(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SetSceneKeyPoint(2);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SetSceneKeyPoint(3);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            SetSceneKeyPoint(4);
+        }
+    }
+
+  
+
+    public void SetSceneKeyPoint(int position)
+    {
+        if (position > -1 && position < positionsList.Count)
+        {
+            player.transform.position = positionsList[position];           
+        }
+
+    }
+
+
+    private void fillTheList()
+    {
+        foreach (GameObject current in PlayerPositionList)
+        {
+            positionsList.Add(current.transform.position);
+        }
+    }
+
+}
