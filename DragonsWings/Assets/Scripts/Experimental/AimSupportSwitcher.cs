00f01c8e0bf671bb0dbcd0d9e1779f608a9788fd
Bürgermeister <freak03@gmx.net>
2018-12-17 16:10:53 +0100

    AimSupport Grafik plus script drin
    Lootraum mehr kisten
    AImsupport grafiken drin
    script für aimsupport unter canvas

---
+++ DragonsWings/Assets/Scripts/Experimental/AimSupportSwitcher.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AimSupportSwitcher : MonoBehaviour
+{
+
+    public Sprite picOn;
+    public Sprite picOff;
+
+    private bool supportIsOn = true;
+
+    public GameObject picture;
+
+
+
+    public void switchPic()
+    {
+        supportIsOn = !supportIsOn;
+
+        if (supportIsOn) picture.GetComponent<Image>().sprite = picOn;
+        else picture.GetComponent<Image>().sprite = picOn;
+    }
+
+
+}
